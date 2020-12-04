66cfb967907c5a9f319db63793b4e4cf65972e7e
GitHub Action <action@github.com>
2020-12-04 21:44:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/leoluk/intellijcue/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leoluk/intellijcue/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leoluk.intellijcue.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leoluk.intellijcue.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/leoluk/intellijcue/services/MyProjectService.kt
+++ src/main/kotlin/com/github/leoluk/intellijcue/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.leoluk.intellijcue.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leoluk.intellijcue.MyBundle
 
 class MyProjectService(project: Project) {
 
